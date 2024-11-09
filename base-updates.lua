ddfe9f4b8562042b4ff5895849a471c220feb8d2
TheXRify <penguinking684@gmail.com>
2024-11-09 00:59:55 -0600

    initial commit

---
+++ base-updates.lua
@@ -0,0 +1,5 @@
+local ruflib = require "__rufmorio-lib__.funcs"
+
+ruflib.remove_recipes()
+ruflib.remove_techs()
+ruflib.remove_ores()
---
+++ data.lua
@@ -0,0 +1,7 @@
+require "__rufmorio-lib__.lib.metas.item"
+require "__rufmorio-lib__.lib.metas.recipe"
+require "__rufmorio-lib__.lib.metas.technology"
+require "__rufmorio-lib__.lib.metas.fluid"
+require "__rufmorio-lib__.lib.metas.entity"
+
+require "base-updates"
